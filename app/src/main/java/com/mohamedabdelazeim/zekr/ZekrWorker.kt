f0129e209e5c080573ff87e2b3332172a1b42c42
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-13 19:29:06 +0200

    Update ZekrWorker.kt

--- app/src/main/java/com/mohamedabdelazeim/zekr/ZekrWorker.kt
+++ app/src/main/java/com/mohamedabdelazeim/zekr/ZekrWorker.kt
@@ -2,13 +2,14 @@ package com.mohamedabdelazeim.zekr
 
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 class ZekrWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
         val intent = Intent(applicationContext, ZekrService::class.java)
-        applicationContext.startForegroundService(intent)
+        ContextCompat.startForegroundService(applicationContext, intent)
         return Result.success()
     }
 }
